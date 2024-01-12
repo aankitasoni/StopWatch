1641998c508d9e3a97d0dd1931b07e9bc29afe89
Ankita Soni <ankitasoni018@gmail.com>
2024-01-13 01:00:58 +0530

    icon launcher added

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'StopWatch',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color.fromRGBO(16, 13, 34, 1),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
